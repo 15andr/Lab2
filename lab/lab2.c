e9aa8f85d20b0223729ec68cf4e3db19e31ed976
Andrei-Daniel Ungureanu <andrei.ungureanu310706@gmail.com>
2026-03-05 13:36:32 +0200

    Fixed bug with recursion

--- lab/lab2.c
+++ lab/lab2.c
@@ -1,20 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int function(int x){
-	x=x-2;
-	x=function(x);
-	return x;
+int function(int x) {
+    if (x <= 0)
+	 return x;   
+    return function(x - 2);
 }
 
-int main(){
-	int a=30, i=0;
-	
-	while (a>0){
-		a=function(a);
-		i++;
-	}
-	printf("%d", i);
-	return 0;	
-}
+int main(void) {
+    int a = 30;
+	int i = 0;
 
+    while (a > 0) {
+        a = function(a);  
+        i++;
+    }
+
+    printf("%d\n", i);
+    return 0;
+}
